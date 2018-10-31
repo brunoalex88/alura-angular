90986371e388be597cbb1dbd438450c9a6c415ac
Bruno Alex de Oliveira <bruno.alex@castmeta.local>
2018-10-31 15:47:22 -0300

    5- 614-typescript-video3.4-template-dinamico

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -4,11 +4,13 @@ class NegociacaoController {
     private _inputQuantidade: HTMLInputElement;
     private _inputValor: HTMLInputElement;
     private _negociacoes = new Negociacoes()
+    private _negociacoesView = new NegociacoesView('#negociacoesView');
 
     constructor() {
         this._inputData = <HTMLInputElement> document.querySelector("#data");
         this._inputQuantidade = <HTMLInputElement> document.querySelector("#quantidade");
         this._inputValor = <HTMLInputElement> document.querySelector("#valor");
+        this._negociacoesView.update(this._negociacoes);
     }
 
     adiciona(event: Event) {
@@ -26,6 +28,8 @@ class NegociacaoController {
             console.log(negociacao.valor);
             console.log(negociacao.quantidade);
         });
+
+        this._negociacoesView.update(this._negociacoes);
     }
 
 }
